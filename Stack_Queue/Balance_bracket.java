5b071e311eabd5ed4b33f77728e939f704a23a40
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2021-05-12 04:49:20 +0530

    stack

--- Stack_Queue/Balance_bracket.java
+++ Stack_Queue/Balance_bracket.java
@@ -1,32 +1,53 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Balance_bracket {
 
     public static void main(String[] args) throws Exception
     {
         Scanner scn = new Scanner(System.in);
 
         String exp = scn.nextLine();
-
-        System.out.println(Balance(exp));
+          boolean b1=Balance(exp);
+        System.out.println(""+b1);
     }
-
+//  ((()
     public static boolean Balance(String str)
     {
+		Stack<Character> st=new Stack<Character>();
 		 for(int i=0;i<str.length();i++)
 		 {
-			 char ch=str.charAt(i);
-			 if(ch=='('||ch=='{'||ch=='[')
-			 {
-				 st.push(ch);
+		  char ch=str.charAt(i);
+		    if(ch=='('||ch=='{'||ch=='[')
+		    {
+			 st.push(ch);
 			 }
-			 else if(st.peek()==')'&&)
-				     {
-					 }
+			 else if(ch==')'||ch=='}'||ch==']')
+			 {
+			     if(st.size()==0)
+			     {
+				    System.out.println("");
+				  }
+				   else if (
+				                       (ch==')'&&st.peek()!='(')||
+				                       (ch=='}'&&st.peek()!='{')||
+				                       (ch==']'&& st.peek()!='[' )
+                    )
+                    {
 
+					  return false;
+					 }
+					 else
+					 {
+					  st.pop();
+					  }
 			 }
 		 }
+		   if(st.size()>0)
+		   {
+			 return false;
+			}
+		 return true;
     }
 
 }
