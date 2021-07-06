a7c18a2f99ee1e8312ba6fc9615a7945444d1d93
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2021-07-06 21:52:29 +0530

    Update Level2.java

--- Level2/Level2.java
+++ Level2/Level2.java
@@ -29,3 +29,33 @@
        oddTail.next=null;///Most Important
        return DummyEven.next;
     }
+
+
+
+public static ListNode removeDuplicates(ListNode head) 
+    {
+        if(head==null||head.next==null) return head;
+        ListNode curr=head.next; //imp
+        ListNode Dummy=new ListNode(-1);
+        ListNode iter=Dummy;
+        iter.next=head;
+        while(curr!=null)
+        {
+            boolean isLooprun=false;
+            while(curr!=null && iter.next.val==curr.val)
+            {
+                isLooprun=true;
+                curr=curr.next;
+            }
+            if(isLooprun)
+            {
+                iter.next=curr;
+            }
+            else
+            {
+             iter=iter.next;   
+            }
+            if(curr!=null)curr=curr.next;///Imp step 1->1->1->N(iteratior point to null) to avoid this we use this
+        }
+            return Dummy.next;
+    }
