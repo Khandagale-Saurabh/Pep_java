7a22d9f77b374d3d78dbb00eb4bbc0ca8c4fc892
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2021-07-07 20:02:06 +0530

    Update Level2.java

--- Level2/Level2.java
+++ Level2/Level2.java
@@ -59,3 +59,58 @@ public static ListNode removeDuplicates(ListNode head)
         }
             return Dummy.next;
     }
+
+
+
+  public static ListNode subtractTwoNumbers(ListNode l1, ListNode l2)
+    {
+    
+        if(l2==null)
+        {
+            return l1;
+        }
+        if(l1==null)
+        {
+         l2.val=-l2.val;
+         return l2;
+        }
+        
+        
+        /*
+        int diff=0;
+        int bow=0;
+               -1
+          1->2->3->4->5
+        - 
+          9->2
+        ==========
+          0/  2
+        */
+        ListNode Dummy =new ListNode(-1);
+        ListNode itr=Dummy;
+        l1=reverse(l1);
+        l2=reverse(l2);
+        ListNode c1=l1;
+        ListNode c2=l2;
+        int borr=0;
+        while(c1!=null)
+        {
+            int diff=borr+c1.val-(c2!=null?c2.val:0);//imp
+            if(diff<0)
+            {
+               diff+=10;//imp
+               borr=-1;
+            }
+            else
+            {
+                borr=0;
+            }
+            ListNode newNode=new ListNode(diff);
+            itr.next=newNode;
+            itr=itr.next;
+            c1=c1.next;
+            if(c2!=null) c2=c2.next;//imp
+        }
+        return reverse(Dummy.next); 
+        
+    }
